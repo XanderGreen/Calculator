73521fbf3ab5eb73365d1b3ec2cef28796354cf1
PDHS lab <pdhslab@DSUSD.K12.CA.US>
2017-01-13 10:49:46 -0800

    added colors, fixed buttons, square root, and pi

--- calculator.py
+++ calculator.py
@@ -1,8 +1,12 @@
+# -*- coding: utf-8 -*-
 from Tkinter import *
 import tkMessageBox
+import math
 
-class Calc():
-    def __init__(self):
+x = input
+
+class Calc(): #making it simpler :)
+    def __init__(self): #how the numbers are entered - starts off with 0
         self.total = 0
         self.current = ""
         self.new_num = True
@@ -11,7 +15,7 @@ class Calc():
         self.eq = False
 
 
-    def num_press(self, num):
+    def num_press(self, num): #coding for button presses
         self.eq = False
         temp = text_box.get()
         temp2 = str(num)      
@@ -25,7 +29,7 @@ class Calc():
             self.current = temp + temp2
         self.display(self.current)
 
-    def calc_total(self):
+    def calc_total(self): #calculate total
         self.eq = True
         self.current = float(self.current)
         if self.op_pending == True:
@@ -33,11 +37,11 @@ class Calc():
         else:
             self.total = float(text_box.get())
 
-    def display(self, value):
+    def display(self, value): #what is displayed in the text box
         text_box.delete(0, END)
         text_box.insert(0, value)
 
-    def do_sum(self):
+    def do_sum(self): #specific functions
         if self.op == "add":
             self.total += self.current
         if self.op == "minus":
@@ -50,7 +54,7 @@ class Calc():
         self.op_pending = False
         self.display(self.total)
 
-    def operation(self, op): 
+    def operation(self, op): #different operations
         self.current = float(self.current)
         if self.op_pending:
             self.do_sum()
@@ -61,79 +65,106 @@ class Calc():
         self.op = op
         self.eq = False
 
-    def cancel(self):
+    def cancel(self): #deletes what was just done
         self.eq = False
         self.current = "0"
         self.display(0)
         self.new_num = True
 
-    def all_cancel(self):
+    def all_cancel(self): #deletes everything
         self.cancel()
         self.total = 0
 
-    def sign(self):
+    def sign(self): #changes sign
         self.eq = False
         self.current = -(float(text_box.get()))
         self.display(self.current)
-
+        
 sum1 = Calc()
 root = Tk()
 calc = Frame(root)
 calc.grid()
 
-root.title("Calculator")
+root.title("Scientific Calculator")
 text_box = Entry(calc, justify=RIGHT)
 text_box.grid(row = 0, column = 0, columnspan = 4, sticky=EW)
 text_box.insert(0, "0")
+text_box.config(bg="black", foreground="white")
 
 numbers = "789456123"
 i = 0
 bttn = []
-for j in range(1,4):
+for j in range(1,4): #builds buttons automatically :D
     for k in range(3):
         bttn.append(Button(calc, text = numbers[i]))
-        bttn[i].grid(row = j, column = k)
+        bttn[i].grid(row = j, column = k, sticky=EW)
+        bttn[i].config(bg="sky blue")
         bttn[i]["command"] = lambda x = numbers[i]: sum1.num_press(x)
         i += 1
 
 bttn_0 = Button(calc, text = "0")
 bttn_0["command"] = lambda: sum1.num_press(0)
-bttn_0.grid(row = 4, column = 1, sticky=EW)
+bttn_0.grid(row = 4, column = 0, sticky=EW, columnspan=2)
+bttn_0.config(bg="sky blue")
 
 bttn_div = Button(calc, text = chr(247))
 bttn_div["command"] = lambda: sum1.operation("divide")
 bttn_div.grid(row = 1, column = 3, sticky=EW)
+bttn_div.config(bg="dodger blue")
 
 bttn_mult = Button(calc, text = "x")
 bttn_mult["command"] = lambda: sum1.operation("times")
 bttn_mult.grid(row = 2, column = 3, sticky=EW)
+bttn_mult.config(bg="dodger blue")
 
 minus = Button(calc, text = "-")
 minus["command"] = lambda: sum1.operation("minus")
 minus.grid(row = 3, column = 3, sticky=EW)
+minus.config(bg="dodger blue")
 
 point = Button(calc, text = ".")
 point["command"] = lambda: sum1.num_press(".")
-point.grid(row = 4, column = 0, sticky=EW)
+point.grid(row = 4, column = 2, sticky=EW)
+point.config(bg="sky blue")
 
 add = Button(calc, text = "+")
 add["command"] = lambda: sum1.operation("add")
 add.grid(row = 4, column = 3, sticky=EW)
+add.config(bg="dodger blue")
 
 neg= Button(calc, text = "+/-")
 neg["command"] = sum1.sign
 neg.grid(row = 5, column = 0, sticky=EW)
+neg.config(bg="dodger blue")
 
 clear = Button(calc, text = "C")
 clear["command"] = sum1.cancel
 clear.grid(row = 5, column = 1, sticky=EW)
+clear.config(bg="dodger blue")
 
 all_clear = Button(calc, text = "AC")
 all_clear["command"] = sum1.all_cancel
 all_clear.grid(row = 5, column = 2, sticky=EW)
+all_clear.config(bg="dodger blue")
 
 equals = Button(calc, text = "=")
 equals["command"] = sum1.calc_total
 equals.grid(row = 5, column = 3, sticky=EW)
+equals.config(bg="dodger blue")
+
+pi = Button(calc, text = "π")
+pi["command"] = math.pi
+pi.grid(row = 1, column = 4, sticky=EW)
+pi.config(bg="deep sky blue")
+
+sqrt = Button(calc, text = "√")
+sqrt["command"] = math.sqrt
+sqrt.grid(row = 4, column = 4, sticky=EW)
+sqrt.config(bg="deep sky blue")
+
+squared = Button(calc, text = "x²")
+squared["command"] = math.pow(x,2.0)
+squared.grid(row = 2, column = 4, sticky=EW)
+squared.congif(bg="deep sky blue")
 
 root.mainloop()
