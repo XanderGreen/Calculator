f6692af286648a6827bb99325a70e471d9f3b8fa
MaddieCW3 <pdhslab@DSUSD.K12.CA.US>
2017-01-23 10:49:51 -0800

    fixed square roots, etc

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from Tkinter import *
 import tkMessageBox
+import Tkinter
 import math
 
 class Calc(): #making it simpler :)
@@ -66,10 +67,21 @@ class Calc(): #making it simpler :)
         
     def do_pi(self):
         self.current = math.pi
-        self.current = float(self.current)
         self.total = self.current
         self.display(self.total)
-
+        
+    def do_cubed(self):
+        self.current = float(self.current)
+        self.current = math.pow(self.current, 3)
+        self.total = self.current
+        self.display(self.total)
+        
+    def do_cbrt(self):
+        self.current = float(self.current)
+        self.current = math.pow(self.current, 1/3)
+        self.total = self.current
+        self.display(self.total)
+        
     def operation(self, op): #different operations
         self.current = float(self.current)
         if self.op_pending:
@@ -97,22 +109,29 @@ class Calc(): #making it simpler :)
         self.display(self.current)
         
 x = int
-        
+ 
+top = Tk()      
 sum1 = Calc()
 root = Tk()
 calc = Frame(root)
 calc.grid()
 
-root.title("Scientific Calculator")
+root.title("Scientific Calculator") 
+
 text_box = Entry(calc, justify=RIGHT)
-text_box.grid(row = 0, column = 0, columnspan = 4, sticky=EW)
+text_box.grid(row = 2, column = 0, columnspan = 5, sticky=EW)
 text_box.insert(0, "0")
 text_box.config(bg="black", foreground="white")
+
+mb=  Menubutton (top, text="File", relief=RAISED )
+mb.grid(row=0, column=0, columnspan=4, sticky=EW)
+mb.menu  =  Menu (mb, tearoff = 0)
+mb["menu"]  =  mb.menu
 
 numbers = "789456123"
 i = 0
 bttn = []
-for j in range(1,4): #builds buttons automatically :D
+for j in range(3,6): #builds buttons automatically :D
     for k in range(3):
         bttn.append(Button(calc, text = numbers[i]))
         bttn[i].grid(row = j, column = k, sticky=EW)
@@ -122,67 +141,77 @@ for j in range(1,4): #builds buttons automatically :D
         
 bttn_0 = Button(calc, text = "0")
 bttn_0["command"] = lambda: sum1.num_press(0)
-bttn_0.grid(row = 4, column = 0, sticky=EW, columnspan=2)
+bttn_0.grid(row = 6, column = 0, sticky=EW, columnspan=2)
 bttn_0.config(bg="sky blue")
 
 bttn_div = Button(calc, text = chr(247))
 bttn_div["command"] = lambda: sum1.operation("divide")
-bttn_div.grid(row = 1, column = 3, sticky=EW)
+bttn_div.grid(row = 3, column = 3, sticky=EW)
 bttn_div.config(bg="dodger blue")
 
 bttn_mult = Button(calc, text = "x")
 bttn_mult["command"] = lambda: sum1.operation("times")
-bttn_mult.grid(row = 2, column = 3, sticky=EW)
+bttn_mult.grid(row = 4, column = 3, sticky=EW)
 bttn_mult.config(bg="dodger blue")
 
 minus = Button(calc, text = "-")
 minus["command"] = lambda: sum1.operation("minus")
-minus.grid(row = 3, column = 3, sticky=EW)
+minus.grid(row = 5, column = 3, sticky=EW)
 minus.config(bg="dodger blue")
 
 point = Button(calc, text = ".")
 point["command"] = lambda: sum1.num_press(".")
-point.grid(row = 4, column = 2, sticky=EW)
+point.grid(row = 6, column = 2, sticky=EW)
 point.config(bg="sky blue")
 
 add = Button(calc, text = "+")
 add["command"] = lambda: sum1.operation("add")
-add.grid(row = 4, column = 3, sticky=EW)
+add.grid(row = 6, column = 3, sticky=EW)
 add.config(bg="dodger blue")
 
 neg= Button(calc, text = "+/-")
 neg["command"] = sum1.sign
-neg.grid(row = 5, column = 0, sticky=EW)
+neg.grid(row = 7, column = 0, sticky=EW)
 neg.config(bg="dodger blue")
 
 clear = Button(calc, text = "C")
 clear["command"] = sum1.cancel
-clear.grid(row = 5, column = 1, sticky=EW)
+clear.grid(row = 7, column = 1, sticky=EW)
 clear.config(bg="dodger blue")
 
 all_clear = Button(calc, text = "AC")
 all_clear["command"] = sum1.all_cancel
-all_clear.grid(row = 5, column = 2, sticky=EW)
+all_clear.grid(row = 7, column = 2, sticky=EW)
 all_clear.config(bg="dodger blue")
 
 equals = Button(calc, text = "=")
 equals["command"] = sum1.calc_total
-equals.grid(row = 5, column = 3, sticky=EW)
+equals.grid(row = 7, column = 3, sticky=EW)
 equals.config(bg="dodger blue")
-
-pi = Button(calc, text = "π")
-pi["command"] = sum1.do_pi
-pi.grid(row = 1, column = 4, sticky=EW)
-pi.config(bg="deep sky blue")
-
-sqrt = Button(calc, text = "√")
-sqrt["command"] = sum1.do_sqrt
-sqrt.grid(row = 4, column = 4, sticky=EW)
-sqrt.config(bg="deep sky blue")
 
 squared = Button(calc, text = "x²")
 squared["command"] = sum1.do_pow
-squared.grid(row = 2, column = 4, sticky=EW)
+squared.grid(row = 3, column = 4, sticky=EW)
 squared.config(bg="deep sky blue")
+
+cubed = Button(calc, text = "x³")
+cubed["command"] = sum1.do_cubed
+cubed.grid(row = 4, column = 4, sticky=EW)
+cubed.config(bg="deep sky blue")
+
+sqrt = Button(calc, text = "√")
+sqrt["command"] = sum1.do_sqrt
+sqrt.grid(row=5, column =4, sticky = EW)
+sqrt.config(bg="deep sky blue")
+
+cbrt = Button(calc, text = "∛")
+cbrt["command"] = sum1.do_cbrt
+cbrt.grid(row=6, column=4, sticky=EW)
+cbrt.config(bg="deep sky blue")
+
+pi = Button(calc, text = "π")
+pi["command"] = sum1.do_pi
+pi.grid(row = 7, column = 4, sticky=EW)
+pi.config(bg="deep sky blue")
 
 root.mainloop()
